3375d2460eb0e7d98fc1a9a89397665d9e3178df
Lyra <jielanjiang46@gmail.com>
2026-06-02 22:51:27 +0800

    add topic and entry

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
    # 主页
    path('', views.index, name='index'),
    path('nav/', views.navigation, name='navigation'),
+   path('topics/',views.topics,name='topics'),
+
+   # 特定主题的详细页面
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
@@ -8,3 +9,16 @@ def index(request):
 
 def navigation(request):
     return render(request, 'main_app/navigation.html')
+
+def topics(request):
+    """显示所有的主题"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'main_app/topics.html', context)
+
+def topic(request, topic_id):
+    """显示单个主题及其所有的条目"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'main_app/topic.html', context)
