1187f9687a377cb5822e9cb53d3cd56f7199223b
Lyra <jielanjiang46@gmail.com>
2026-06-05 21:52:25 +0800

    add edit function

--- dmbj_project/settings.py
+++ dmbj_project/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'  # 设置默认时区为上海（即北京时间）
 
 USE_I18N = True
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Topic
+from django.shortcuts import render, redirect
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -22,3 +23,57 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'main_app/topic.html', context)
+
+def new_topic(request):
+    """添加新主题"""
+    if request.method != 'POST':
+        # 未提交数据：创建一个新表单
+        form = TopicForm()
+    else:
+        # POST 提交的数据：对数据进行处理
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('main_app:topics')
+
+    # 显示空表单或指出表单数据无效
+    context = {'form': form}
+    return render(request, 'main_app/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """在特定主题中添加新条目"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # 未提交数据：创建一个空表单
+        form = EntryForm()
+    else:
+        # POST 提交的数据：对数据进行处理
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('main_app:topic', topic_id=topic_id)
+
+    # 显示空表单或指出表单数据无效
+    context = {'topic': topic, 'form': form}
+    return render(request, 'main_app/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """编辑既有的条目"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # 初次请求：使用当前的条目填充表单
+        form = EntryForm(instance=entry)
+    else:
+        # POST 提交的数据：对数据进行处理
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+          form.save()
+          return redirect('main_app:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'main_app/edit_entry.html', context)
